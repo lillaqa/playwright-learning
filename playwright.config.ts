8fc69f17ded201a7b83ebeef1f89fe28764c7919
Lilla-UIC <89205874+Lilla-UIC@users.noreply.github.com>
2026-02-22 15:20:44 +0100

    timeout settings changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 30_000,
+  globalTimeout: 10 * 60 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
