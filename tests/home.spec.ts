0809068157e8c8eb9924f59c80180fd41fb36cec
lillaqa <kovilil@gmail.com>
2026-02-23 14:28:25 +0100

    small fix

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test("Homen page", async ({ page }) => {
+test("Home page", async ({ page }) => {
     await page.goto('https://practicesoftwaretesting.com/');
     await expect(page.getByTitle('Practice Software Testing - Toolshop - v5.0')).toBeVisible();
     await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
