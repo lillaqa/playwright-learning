fbda16982bf6fb60836fe948af62feda18a7c092
lillaqa <kovilil@gmail.com>
2026-02-23 15:00:55 +0100

    reorganize tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,17 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test("Home page", async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com/');
-    await expect(page.getByTitle('Practice Software Testing - Toolshop - v5.0')).toBeVisible();
-    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+test.describe('Home page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://practicesoftwaretesting.com/');
+    });
+    test("Validate sign in", async ({ page }) => {
+            await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+    });
 
-    const productGrid = page.locator(".col-md-9");
-    await expect(productGrid.getByRole('link')).toHaveCount(9);
-    //value alternative
-    expect(await productGrid.getByRole('link').count()).toBe(9);
+    test("Validate title", async ({ page }) => {
+        await expect(page.getByTitle('Practice Software Testing - Toolshop - v5.0')).toBeVisible();
+    });
 
-    //challenge: search for thor hammer (action), check the result in the grid (assertion)
-    await page.getByPlaceholder('Search for products').fill('Thor Hammer');
-    await page.getByRole('button', { name: 'Search' }).click();
-    await expect(productGrid.getByRole('link')).toHaveCount(1);
-})
+    test("Validate product grid", async ({ page }) => {
+        const productGrid = page.locator(".col-md-9");
+        await expect(productGrid.getByRole('link')).toHaveCount(9);
+        //value alternative
+        //expect(await productGrid.getByRole('link').count()).toBe(9);
+    });
+
+    test("Validate search", async ({ page }) => {
+        const productGrid = page.locator(".col-md-9");
+        await page.getByPlaceholder('Search for products').fill('Thor Hammer');
+        await page.getByRole('button', { name: 'Search' }).click();
+        await expect(productGrid.getByRole('link')).toHaveCount(1);
+    });
+
+});
