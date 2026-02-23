1f5372414673e86a0de154106e83653a9fab971e
lillaqa <kovilil@gmail.com>
2026-02-23 16:25:33 +0100

    authentication set up and test

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,1 +1,15 @@
 import { test as setup, expect } from '@playwright/test';
+
+setup("Create customer nr. 1", async ({ page, context }) => {
+    const email = "customer@practicesoftwaretesting.com";
+    const password = "welcome01";
+    const customer1Auth = ".auth/customer1.json";
+
+    await page.goto("https://practicesoftwaretesting.com/auth/login");
+    await page.getByTestId("email").fill(email);
+    await page.getByTestId("password").fill(password);
+    await page.getByTestId("login-submit").click();
+
+    await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+    await context.storageState({ path: customer1Auth });
+});
