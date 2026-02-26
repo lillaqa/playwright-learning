0dcff3a2f67420e4eeb360dbe69120036465e825
lillaqa <kovilil@gmail.com>
2026-02-26 17:21:03 +0100

    tests for buttons

--- tests/product-pages/product-page.spec.ts
+++ tests/product-pages/product-page.spec.ts
@@ -24,4 +24,26 @@ test.describe('Validate product page without authentication', () => {
         await expect(page.getByTestId('add-to-favorites')).toBeVisible();
         await expect(page.getByTestId('add-to-favorites')).toBeEnabled();
     });
+
+    test('Validate quantity selector is visible and enabled', async ({page}) => {
+        await expect(page.getByTestId('decrease-quantity')).toBeVisible();
+        await expect(page.getByTestId('decrease-quantity')).toBeEnabled();
+        await expect(page.getByTestId('quantity')).toHaveValue('1');
+        await expect(page.getByTestId('increase-quantity')).toBeVisible();
+        await expect(page.getByTestId('increase-quantity')).toBeEnabled();
+    });
+
+    test('Validate add to favorite button functionality as a guest', async ({page}) => {
+        await page.getByTestId('add-to-favorites').click();
+        await expect(page.getByRole('alert', {name: 'Unauthorized, can not add' })).toBeVisible();
+        await expect(page.getByRole('alert', {name: 'Unauthorized, can not add' })).toHaveText('Unauthorized, can not add product to your favorite list.');
+    });
+
+    test('Validate add to cart button functionality as a guest', async ({page}) => {
+        await page.getByTestId('add-to-cart').click();
+        await expect(page.getByRole('alert', {name: 'Product added to shopping' })).toBeVisible();
+        await expect(page.getByRole('alert', {name: 'Product added to shopping' })).toHaveText('Product added to shopping cart.');
+        await expect(page.getByTestId('cart-quantity')).toHaveText('1');
+    });
+
 });
