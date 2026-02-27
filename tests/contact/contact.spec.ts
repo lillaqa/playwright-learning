89c3773ef25d5104172d3c93233d170ea9a3ae1d
lillaqa <kovilil@gmail.com>
2026-02-27 11:25:11 +0100

    Update contact.spec.ts
    contact tests - remove comments

--- tests/contact/contact.spec.ts
+++ tests/contact/contact.spec.ts
@@ -1,28 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-// test('test', async ({ page }) => {
-//   await page.goto('https://practicesoftwaretesting.com/');
-//   await page.getByRole('slider', { name: 'ngx-slider', exact: true }).click();
-//   await page.getByRole('slider', { name: 'ngx-slider-max' }).click();
-//   await page.getByRole('slider', { name: 'ngx-slider', exact: true }).click();
-//   await page.getByRole('slider', { name: 'ngx-slider-max' }).click();
-//   await page.getByTestId('nav-contact').click();
-//   await page.getByTestId('first-name').click();
-//   await page.getByTestId('first-name').fill('test');
-//   await page.getByTestId('last-name').click();
-//   await page.getByTestId('last-name').fill('demo');
-//   await page.getByTestId('email').click();
-//   await page.getByTestId('email').fill('demo');
-//   await page.getByTestId('email').press('Alt+@');
-//   await page.getByTestId('email').fill('demodemo.io');
-//   await page.getByTestId('subject').selectOption('return');
-//   await page.getByTestId('message').click();
-//   await page.getByTestId('message').fill('hsdfhgjh sjhdgj hdjhgjhdgjh djfhgjhdfgjh fjghdj gjhdjghdjhgdjkhfgjhdfjhgdjhfgjhdgjh dfjhgjdfhgjhdjhgjfdhsjhg jfdhgjhsdgjhruzt ukdsgkdjd');
-//   await page.getByTestId('contact-submit').click();
-//   await page.getByTestId('email').click();
-//   await page.getByText('Email format is invalid').click();
-//   await page.getByTestId('nav-contact').click();
-// });
 
 test.describe('Check that contact opens', () => {
   test('contact opens', async ({ page }) => {
