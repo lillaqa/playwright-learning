a57561ea32aaaf7e7b64f351887edcfa3cc24274
lillaqa <kovilil@gmail.com>
2026-02-27 11:59:30 +0100

    ucontact - ui text element visible and placeholder tests

--- tests/contact/contact.spec.ts
+++ tests/contact/contact.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-
 test.describe('Check that contact opens', () => {
   test('contact opens', async ({ page }) => {
     await page.goto('https://practicesoftwaretesting.com/');
@@ -14,12 +13,20 @@ test.describe('Validte contact form', () => {
     await page.goto('https://practicesoftwaretesting.com/contact');
   });
 
-  test('Validate texts', async ({ page }) => {
+  test('Validate  elements', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Contact' })).toHaveText('Contact');
     await expect(page.getByText('First name')).toBeVisible();
     await expect(page.getByText('Last name')).toBeVisible();
     await expect(page.getByText('Email address')).toBeVisible();
-    await expect(page.getByText('Subject')).toBeVisible();
-    await expect(page.getByText('Message')).toBeVisible();
+    await expect(page.getByText('Subject', {exact: true})).toBeVisible();
+    await expect(page.getByText('Message *')).toBeVisible();
     await expect(page.getByText('Attachment')).toBeVisible();
+  });
+
+  test('Validate placeholder texts', async ({ page }) => {
+    await expect(page.locator('[data-test="first-name"]')).toHaveAttribute('placeholder', 'Your first name *');
+    await expect(page.locator('[data-test="last-name"]')).toHaveAttribute('placeholder', 'Your last name *');
+    await expect(page.locator('[data-test="email"]')).toHaveAttribute('placeholder', 'Your email *');
+    await expect(page.locator('[data-test="subject"]')).toHaveAttribute('placeholder', 'Select a subject *');
   });
 });
