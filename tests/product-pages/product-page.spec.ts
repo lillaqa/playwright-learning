ac642a69ff488ac7d2ae9582730bccaf606e8c9f
lillaqa <kovilil@gmail.com>
2026-03-02 20:54:39 +0100

    add footer and header element tests to product page test

--- tests/product-pages/product-page.spec.ts
+++ tests/product-pages/product-page.spec.ts
@@ -1,10 +1,16 @@
 import {test, expect} from '@playwright/test';
+import FooterPage from '../../pages/footer-page';
+import HeaderPage from '../../pages/header-page';
+
+let footerPage: FooterPage;
+let headerPage: HeaderPage;
+
+test.beforeEach(async ({page}) => {
+    await page.goto('https://practicesoftwaretesting.com/');
+    await page.getByText('Slip Joint Pliers').click();
+});
 
 test.describe('Validate product page without authentication', () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto('https://practicesoftwaretesting.com/');
-        await page.getByText('Slip Joint Pliers').click();
-    });
 
     test('Validate pruduct detail text elements', async ({page}) => {
         await expect(page.getByTestId('product-name')).toHaveText('Slip Joint Pliers');
@@ -68,3 +74,28 @@ test.describe('Validate product page with authenticated user', () => {
         await expect(page.getByTestId('cart-quantity')).toHaveText('1');
     });
 });
+
+test.describe('Validate global UI elements on product page', () => {
+  test.beforeEach(async ({ page }) => {
+    footerPage = new FooterPage(page);
+    headerPage = new HeaderPage(page);
+  });
+
+  test('Validate header elements: notification bar', async ({ page }) => {
+    await headerPage.validateNotificationBar();
+    await headerPage.validateDocumentationLink();
+  });
+
+  test('Validate header elements: app header', async ({ page }) => {
+    await headerPage.validateAppHeader();
+  });
+
+  test('Validate footer is visible and text is correct', async ({ page }) => {
+    await footerPage.validateFooterText();
+    await footerPage.validateGitHubLink();
+    await footerPage.validatePrivacyPolicyLink();
+    await footerPage.validateBarnImagesLink();
+    await footerPage.validateUnsplashLink();
+  });
+});
+
