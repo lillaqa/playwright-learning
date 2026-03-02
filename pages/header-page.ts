0eb5a64c5e68c08cc8881c89b1694f4674b4056b
lillaqa <kovilil@gmail.com>
2026-03-02 16:32:31 +0100

    start header object file

---
+++ pages/header-page.ts
@@ -0,0 +1,17 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class HeaderPage {
+    //variables
+    readonly page: Page;
+    
+
+    //constructor
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    //methods
+
+}
+
+export default HeaderPage;
