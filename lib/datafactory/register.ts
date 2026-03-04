20683d42fb12102cc009dff221412b32c86b2781
lillaqa <kovilil@gmail.com>
2026-03-04 14:37:43 +0100

    init data factory practice

---
+++ lib/datafactory/register.ts
@@ -0,0 +1,26 @@
+import { expect, request } from '@playwright/test';
+
+export async function registerUser(email: string, password: string) {
+    const apiURL = process.env.API_URL;
+    const crearequestContext = await request.newContext();
+    const response = await crearequestContext.post(apiURL + "/users/register", {
+        data: {
+            first_name: "John",
+            last_name: "Doe",
+            dob: "1990-01-01",
+            phone: "1234567890",
+            email: email,
+            password: password,
+             address: {
+                street: "101 Testing Way",
+                city: "New York",
+                state: "New York",
+                country: "US",
+                postal_code: "55555",
+            },
+        }
+    });
+
+    expect(response.status()).toBe(201);
+    return response.status();
+}
