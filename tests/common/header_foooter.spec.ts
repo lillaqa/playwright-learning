0e5939b0b48903635bc4411c1c55d34061f65dde
lillaqa <kovilil@gmail.com>
2026-03-06 19:27:18 +0100

    reorganize common ui tests part 2

--- tests/common/header_foooter.spec.ts
+++ tests/common/header_foooter.spec.ts
@@ -29,3 +29,29 @@ test.describe('Validate global UI elements on contact page', () => {
     await footerPage.validateUnsplashLink();
   });
 });
+
+test.describe('Validate global UI elements on product page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://practicesoftwaretesting.com/');
+    await page.getByText('Slip Joint Pliers').click();
+    footerPage = new FooterPage(page);
+    headerPage = new HeaderPage(page);
+  });
+
+  test('Validate header elements: notification bar', async ({ page }) => {
+    await headerPage.validateNotificationBar();
+    await headerPage.validateDocumentationLink();
+  });
+
+  test('Validate header elements: app header', async ({ page }) => {
+    await headerPage.validateAppHeader();
+  });
+
+  test('Validate footer is visible and text is correct', async ({ page }) => {
+    await footerPage.validateFooterText();
+    await footerPage.validateGitHubLink();
+    await footerPage.validatePrivacyPolicyLink();
+    await footerPage.validateBarnImagesLink();
+    await footerPage.validateUnsplashLink();
+  });
+});
