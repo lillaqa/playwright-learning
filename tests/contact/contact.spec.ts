7f6b01a18e90656f8862f4041adf2fb7175c395a
lillaqa <kovilil@gmail.com>
2026-03-06 19:23:13 +0100

    reorganize common ui element tests

--- tests/contact/contact.spec.ts
+++ tests/contact/contact.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
-import FooterPage from '@pages/footer.page';
 import HeaderPage from '@pages/header.page';
 
-let footerPage: FooterPage;
 let headerPage: HeaderPage;
 
 test.beforeEach(async ({ page }) => {
@@ -83,27 +81,5 @@ test.describe('Test mandatory fields', () => {
 
 // });
 
-test.describe('Validate global UI elements on contact page', () => {
-  test.beforeEach(async ({ page }) => {
-    footerPage = new FooterPage(page);
-    headerPage = new HeaderPage(page);
-  });
 
-  test('Validate header elements: notification bar', async ({ page }) => {
-    await headerPage.validateNotificationBar();
-    await headerPage.validateDocumentationLink();
-  });
-
-  test('Validate header elements: app header', async ({ page }) => {
-    await headerPage.validateAppHeader();
-  });
-
-  test('Validate footer is visible and text is correct', async ({ page }) => {
-    await footerPage.validateFooterText();
-    await footerPage.validateGitHubLink();
-    await footerPage.validatePrivacyPolicyLink();
-    await footerPage.validateBarnImagesLink();
-    await footerPage.validateUnsplashLink();
-  });
-});
 
