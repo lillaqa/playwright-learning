db7dc21609420fc614a9854ee72e94b4c9007110
lillaqa <kovilil@gmail.com>
2026-03-07 16:06:27 +0100

    reorganize tests, write test for banner

--- tests/home-page/home-noAuth.spec.ts
+++ tests/home-page/home-noAuth.spec.ts
@@ -9,15 +9,27 @@ test.beforeEach(async ({ page }) => {
     await homePage.goto();
 });
 
-test.describe('Home page without authentication', () => {
-   
-    test("Validate sign in", async ({ page }) => {
-            await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
-    });
-
+test.describe('Validate home page header', () => {
     test("Validate title", async ({ page }) => {
         await expect(page.getByTitle('Practice Software Testing - Toolshop - v5.0')).toBeVisible();
     });
+
+    test("Validate logo", async ({ page }) => {
+        //await expect(page.getByRole('img', { name: 'Practice Software Testing' })).toBeVisible();
+        await expect(page.getByRole('link', { name: 'Practice Software Testing -' })).toBeVisible();
+    });
+
+        test("Validate sign in", async ({ page }) => {
+            await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+    });
+
+    test('Validate banner', async ({ page }) => {
+        await expect(homePage.banner).toBeVisible();
+    });
+});
+
+test.describe('Validate home page main sections', () => {
+
 
     test("Validate product grid", async ({ page }) => {
         await expect(homePage.productGrid.getByRole('link')).toHaveCount(9);
@@ -30,5 +42,9 @@ test.describe('Home page without authentication', () => {
         await page.getByRole('button', { name: 'Search' }).click();
         await expect(homePage.productGrid.getByRole('link')).toHaveCount(1);
     });
+
+});
+
+test.describe('Validate sidebar', () => {
 
 });
