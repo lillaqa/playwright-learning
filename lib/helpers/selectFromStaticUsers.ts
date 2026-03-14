0749b7a3fae24c73b190d75c1626a5b8b20f1574
lillaqa <kovilil@gmail.com>
2026-03-14 15:37:28 +0100

    fix setup test - now running

--- lib/helpers/selectFromStaticUsers.ts
+++ lib/helpers/selectFromStaticUsers.ts
@@ -1,9 +1,9 @@
 export const pickStaticUser = () => {
-    const pickStaticUser = [
+    const staticUser = [
         { email: 'customer@practicesoftwaretesting.com', password: 'welcome01', name: 'Jane Doe' },
         { email: 'customer2@practicesoftwaretesting.com', password: 'welcome01', name: 'Jack Howe' },
         { email: 'customer3@practicesoftwaretesting.com', password: 'pass123', name: 'Bob Smith' }
     ];
-    return pickStaticUser[Math.floor(Math.random() * pickStaticUser.length)];
+    return staticUser[Math.floor(Math.random() * staticUser.length)];
 }
 
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,17 +2,14 @@ import { test as setup, expect } from '@playwright/test';
 import { pickStaticUser } from '@helpers/selectFromStaticUsers';
 
 setup("Create customer nr. 1", async ({ page, context }) => {
-    const email = pickStaticUser().email;
-    const password = pickStaticUser().password;
-    const name = pickStaticUser().name
+    const user = pickStaticUser();
     const customer1Auth = ".auth/customer1.json";
 
     await page.goto("https://practicesoftwaretesting.com/auth/login");
-    await page.getByTestId("email").fill(email);
-    await page.getByTestId("password").fill(password);
+    await page.getByTestId("email").fill(user.email);
+    await page.getByTestId("password").fill(user.password);
     await page.getByTestId("login-submit").click();
 
-    await expect(page.getByTestId("nav-menu")).toContainText(name);
+    await expect(page.getByRole('menubar')).toContainText(user.name);
     await context.storageState({ path: customer1Auth });
 });
-
