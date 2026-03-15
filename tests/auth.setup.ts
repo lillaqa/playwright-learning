197409a138de7e553657d9a993b291806420b966
lillaqa <kovilil@gmail.com>
2026-03-15 22:28:06 +0100

    disable helper for experiement -- CI issues

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,14 +2,20 @@ import { test as setup, expect } from '@playwright/test';
 import { pickStaticUser } from '@helpers/selectFromStaticUsers';
 
 setup("Create customer nr. 1", async ({ page, context }) => {
-    const user = pickStaticUser();
+    //const user = pickStaticUser();
     const customer1Auth = ".auth/customer1.json";
 
     await page.goto("https://practicesoftwaretesting.com/auth/login"); 
-    await page.locator('[data-test="email"]').fill(user.email);
-    await page.getByTestId("password").fill(user.password);
+    await page.locator('[data-test="email"]').fill("customer@practicesoftwaretesting.com");
+    await page.getByTestId("password").fill("welcome01");
     await page.getByTestId("login-submit").click();
 
-    await expect(page.getByRole('menubar')).toContainText(user.name);
+    await expect(page.getByRole('menubar')).toContainText("Jane Doe");
+
+    // await page.locator('[data-test="email"]').fill(user.email);
+    // await page.getByTestId("password").fill(user.password);
+    // await page.getByTestId("login-submit").click();
+
+    // await expect(page.getByRole('menubar')).toContainText(user.name);
     await context.storageState({ path: customer1Auth });
 });
