5ce995d5fc01df1f0864467a0a8b0b7c7f94a783
lillaqa <kovilil@gmail.com>
2026-03-16 12:24:50 +0100

    resotre original setup test

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,22 +2,22 @@ import { test as setup, expect } from '@playwright/test';
 import { pickStaticUser } from '@helpers/selectFromStaticUsers';
 
 setup("Create customer nr. 1", async ({ page, context }) => {
-    //const user = pickStaticUser();
+    const user = pickStaticUser();
     const customer1Auth = ".auth/customer1.json";
 
     await page.goto("https://practicesoftwaretesting.com/auth/login"); 
-    const emailInput = page.locator('[data-test="email"]');
-    await emailInput.waitFor({ state: 'visible' }); 
-    await emailInput.fill("customer@practicesoftwaretesting.com");
-    await page.getByTestId("password").fill("welcome01");
-    await page.getByTestId("login-submit").click();
-
-    await expect(page.getByRole('menubar')).toContainText("Jane Doe");
-
-    // await page.locator('[data-test="email"]').fill(user.email);
-    // await page.getByTestId("password").fill(user.password);
+    // const emailInput = page.locator('[data-test="email"]');
+    // await emailInput.waitFor({ state: 'visible' }); 
+    // await emailInput.fill("customer@practicesoftwaretesting.com");
+    // await page.getByTestId("password").fill("welcome01");
     // await page.getByTestId("login-submit").click();
 
-    // await expect(page.getByRole('menubar')).toContainText(user.name);
+    // await expect(page.getByRole('menubar')).toContainText("Jane Doe");
+
+    await page.locator('[data-test="email"]').fill(user.email);
+    await page.getByTestId("password").fill(user.password);
+    await page.getByTestId("login-submit").click();
+
+    await expect(page.getByRole('menubar')).toContainText(user.name);
     await context.storageState({ path: customer1Auth });
 });
