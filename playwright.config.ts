19e5962436be8287b7aa20c81656da2ba3fc9af2
lillaqa <kovilil@gmail.com>
2026-03-16 12:22:59 +0100

    useragent for CI securitiy issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   timeout: 60_000,
-  use: {
-    launchOptions: {
-      slowMo: 50,
-    },
-  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -37,6 +32,10 @@ export default defineConfig({
     testIdAttribute: "data-test",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+      slowMo: 50,
+    },
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
   },
 
   /* Configure projects for major browsers */
