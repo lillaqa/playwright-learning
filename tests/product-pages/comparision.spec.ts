306ef010c3ceb5ddc8f52bcf6357212b5b11e3b8
lillaqa <kovilil@gmail.com>
2026-04-20 20:36:54 +0200

    icon click works

--- tests/product-pages/comparision.spec.ts
+++ tests/product-pages/comparision.spec.ts
@@ -7,3 +7,12 @@ test.beforeEach(async ({ page }) => {
     comparisionPage = new ComparisionPage(page);
     await page.goto('https://practicesoftwaretesting.com/');
 });
+
+test.describe('Home page buttons function as expected', () => {
+    test('Compare button selects the product', async () => {
+        await comparisionPage.compareButton.nth(0).click();
+        await expect(comparisionPage.compareButton.nth(0)).toHaveAttribute('aria-pressed', 'true');
+    });
+
+});
+
