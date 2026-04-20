eea9b8a543d6ab146ebbd37850ab5859d6757108
lillaqa <kovilil@gmail.com>
2026-04-20 23:13:35 +0200

    visual test part 1

--- tests/product-pages/comparision.spec.ts
+++ tests/product-pages/comparision.spec.ts
@@ -9,6 +9,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Home page buttons function as expected', () => {
+    test('Verify that comparison related UI elements are visible on the home page', async () => {
+        await comparisonPage.compareButton.nth(0).click();
+        await expect(comparisonPage.comparisonBar).toBeVisible();
+        await expect(comparisonPage.compareNowButton).toBeVisible();
+        await expect(comparisonPage.homeClearAllButton).toBeVisible();
+    });
+
     test('Compare button selects the product', async () => {
         await comparisonPage.compareButton.nth(0).click();
         await expect(comparisonPage.compareButton.nth(0)).toHaveAttribute('aria-pressed', 'true');
@@ -19,6 +26,7 @@ test.describe('Home page buttons function as expected', () => {
 
     test('Clear All button clears the selected products', async () => {
         await comparisonPage.compareButton.nth(0).click();
+        await expect(comparisonPage.compareButton.nth(0)).toHaveAttribute('aria-pressed', 'true');
         await comparisonPage.compareButton.nth(1).click();
         await comparisonPage.homeClearAllButton.click();
         await expect(comparisonPage.compareButton.nth(0)).toHaveAttribute('aria-pressed', 'false');
